15b3f356f66047f26bb2511f536ba51532cd087e
faizdev007 <faizdev007@gmail.com>
2025-07-15 14:48:07 +0530

    reduce the hero bg size for fast loading

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,7 +11,7 @@
 <link rel="apple-touch-icon" href="{{asset('/favicon.svg')}}">
 
 <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-<link rel="stylesheet" href="{{ asset('build/assets/app-CX9C-JIs.css') }}">
+<link rel="stylesheet" href="{{ asset('build/assets/app-DpmhgVSg.css') }}">
 <script src="{{ asset('build/assets/app-l0sNRNKZ.js') }}" defer></script>
 @fluxAppearance
 
