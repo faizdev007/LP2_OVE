c83167e7864cec3f39849e552f1f7fd01471f871
faizdev007 <faizdev007@gmail.com>
2025-07-15 13:06:08 +0530

    update thankyou page

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.page.bacancy :title="__('Dashboard')">
+<x-layouts.page.bacancy :title="__('Thankyou')" :seo="$seo??['metaTitle'=>'','metaDescription'=>'']">
     <div class="fixed gap-4 top-0 bottom-0 start-0 end-0 z-40 h-full flex justify-center items-center flex-col text-center p-6 bg-bacancy-primary border rounded">
         <h2 class="md:text-5xl text-3xl text-white font-bold font-mono">Thank You!</h2>
         <p class="text-white mt-2 text-xl">Thanks for your query. Our representative will get in touch with you soon!</p>
